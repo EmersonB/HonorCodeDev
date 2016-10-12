2c63ab18f52ce590a34bc02add913efb31ec8e80
EmersonB <eberlik@gmail.com>
2016-10-12 15:33:05 -0400

    first commit

---
+++ src/components/Notes.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import * as firebase from 'firebase'
+
+class Notes extends Component {
+    constructor(props, context){
+        super(props,context)
+        this.updateNote = this.updateNote.bind(this)
+        this.submitNote = this.submitNote.bind(this)
+        this.state = {
+            note: '',
+            notes: []
+        }
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount')
+        firebase.database().ref('notes/').on('value',(snapshot)=> {
+
+            const currentNotes = snapshot.val()
+
+            if(currentNotes != null){
+            this.setState({
+                notes: currentNotes
+            })
+        }
+    }
+    )
+    }
+
+    updateNote(event){
+        console.log('updateNote:'+event.target.value)
+        this.setState({
+            note:event.target.value
+        })
+    }
+
+    submitNote(event){
+        console.log('submitNote: '+this.state.note)
+        const nextNote = {
+            id: this.state.notes.length,
+            text: this.state.note
+        }
+        firebase.database().ref('notes/'+nextNote.id).set(nextNote)
+        // var list = Object.assign([], this.state.messages)
+        // list.push(nextMessage)
+        // this.setState({
+        //     messages: list
+        // })
+    }
+    render(){
+        const currentNotes = this.state.notes.map((note,i) =>{
+                return (
+            <li key = {note.id}>{note.text}</li>
+    )
+    })
+        return (
+            <div>
+            <ol>
+            {currentNotes}
+            </ol>
+            <textarea onChange={this.updateNote} type="text" placeholder="Note"/>
+            <br />
+            <button onClick={this.submitNote}> Submit Note </button>
+        </div>
+    )
+
+    }
+}
+
+export default Notes
---
+++ src/components/fileSubmit.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import Dropzone from 'react-dropzone'
+import * as firebase from 'firebase'
+
+class MyDropzone extends Component {
+    constructor(props,context) {
+        super(props,context);
+        this.onDrop = this.onDrop.bind(this);
+        this.state = {
+            files: []
+        }
+
+    }
+    componentDidMount(){
+        console.log('componentDidMount')
+        firebase.database().ref('files/').on('value',(snapshot)=> {
+
+            const currentFiles = snapshot.val()
+
+            if(currentFiles != null){
+            this.setState({
+                files: currentFiles
+            })
+        }
+    })}
+
+    onDrop(files) {
+        console.log('Received files:',files)
+        for( var x = 0; x<files.length; x=x+1)
+        {
+            const nextFile = {
+                id: this.state.files.length+x,
+                item: files[x]
+            }
+            firebase.database().ref('files/' + nextFile.id).set(nextFile.item)
+        }
+    }
+    render(){
+        const files = this.state.files.map((file,i) =>{
+                return (
+            <li key = {file.id}>{file.preview}</li>
+    )
+    })
+        return (
+            <div>
+            <ol>
+            {files}
+            </ol>
+            <Dropzone onDrop={this.onDrop}>
+            <div>Try dropping some files here, or click to select files to upload.</div>
+            </Dropzone>
+            </div>
+    );
+    }
+}
+
+export default MyDropzone;
